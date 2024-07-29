2ba4845bc1ea780a9cc51d2b56a10f36d14aad9e
TanNguyen20 <nguyennhattan1562000@gmail.com>
2024-07-29 22:33:28 +0700

    add api doc for gateway

---
+++ src/main/java/vn/tannguyen/gateway_service/configs/APIConfiguaration.java
@@ -0,0 +1,34 @@
+package vn.tannguyen.gateway_service.configs;
+
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties;
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
+import org.springframework.cloud.gateway.server.mvc.config.GatewayMvcProperties;
+import org.springframework.cloud.gateway.server.mvc.config.RouteProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+public class APIConfiguaration {
+    @Bean
+    @Lazy(false)
+    public Set<AbstractSwaggerUiConfigProperties.SwaggerUrl> apis(GatewayMvcProperties properties,
+                                                                  SwaggerUiConfigParameters swaggerUiConfigParameters) {
+        Set<AbstractSwaggerUiConfigProperties.SwaggerUrl> urls = new HashSet<>();
+        List<RouteProperties> routes = properties.getRoutes();
+
+        routes.stream().filter(route -> route.getId().matches(".*-service")).forEach(route -> {
+            String name = route.getId().replaceAll("-service", "");
+            AbstractSwaggerUiConfigProperties.SwaggerUrl swaggerUrl = new AbstractSwaggerUiConfigProperties.SwaggerUrl(name, "/" + name, null);
+            urls.add(swaggerUrl);
+        });
+
+        swaggerUiConfigParameters.setUrls(urls);
+        return urls;
+    }
+
+}
